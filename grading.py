b43f5ca67cfbc25d28a78d8d0835b3640da79d25
Georgeo00 <grigorijsmagars@gmail.com>
2026-02-03 12:32:24 +0100

    connecting

--- grading.py
+++ grading.py
@@ -1,37 +1,32 @@
 import json
+import os
 
 
 #task1 (Grigorijs)
-with open("login_data.json", "r", encoding="utf-8") as f:
-    login_data = json.load(f)
+if os.path.exists("login_data.json"):
+    with open("login_data.json", "r", encoding="utf-8") as f:
+        login_data = json.load(f)
+
+if os.path.exists("grading_data.json"):
+    with open("grading_data.json", "r") as file:
+        student_data = json.load(file)    
 
 
 
-def login_true(username, password, users):
-    for user in users:
-        if user["login"] == username and user["password"] == password:
-            return True
-    return False
+def user_role(username, password, users):
+    for admin in users["admins"]:
+        if admin["username"] == username and admin["password"] == password:
+            return "admin"
 
-username_input = input("Enter username:")
-password_input = input("Enter password:")
+    for student in users["students"]:
+        if student["username"] == username and student["password"] == password:
+            return "student"
 
-if login_true(username_input, password_input, login_data):
-    print("login successful")
-else:
-    print("Access denied")
 
-# while login_true:
+
+
 #task 2 (Daksh)
 
-import json
-import os
-def load_students():
-    if os.path.exists("students.json"):
-        with open("students.json", "r") as file:
-            return json.load(file)
-    else:
-        return {}
 class StudentManager:
 
     def __init__(self, data):
@@ -44,26 +39,21 @@ class StudentManager:
         else:
             print("✅ Student not found. You can add this student.")
             return False
-students = load_students()
 
-manager = StudentManager(students)
+#student_id = input("Enter Student ID: ")#could change so that it check for the name instead, because admin(teacher) will probably not know the id of a student (Greg)
+#manager = StudentManager(student_data)
 
-student_id = input("Enter Student ID: ")
-
-manager.student_exists(student_id)
+#manager.student_exists(student_id)
 
 #task3(Dev)
-f= open("grading_data.json","r")
-grade_data=json.load(f)
-f.close()
 def add_grade():
     stuid=int(input("enter student id:"))
     marks=int(input("enter new grade:"))
-    for student in grade_data["students"]:
+    for student in student_data["students"]:
         if student ["id"]==stuid:
             student["grades"]= student["grades"]+marks
             f= open("grading_data.json","w")
-            json.dump(grade_data,f,indent=4)
+            json.dump(student_data,f,indent=4)
             f.close()
             print("grade added successfully")#use print instead of printf in python
         else:
@@ -73,32 +63,22 @@ def add_grade():
                  # add grade
 
 # task 4 (Neetee)adding students to Database using input
-import json
-def load_students():
-    try:
-        with open("grading_py.json", "r", encoding="utf-8") as f:
-            return json.load(f)
-    except FileNotFoundError:
-        return []
-def save_students(students):
+def save_students(student_data):
     with open("grading_py.json", "w", encoding="utf-8") as f:
-        json.dump(students, f, indent=4)
+        json.dump(student_data, f, indent=4)
 
-
-
-def add_student():
-    students = load_students("grading_py.json")
+def add_student():                              #add checking if student already exists (from daksh code)
     name = input("Enter student name: ")
     age = input("Enter student age: ")
-    grade = input("Enter student grade: ")
-    next_id = len(students) + 1
+    grade = input("Enter student grade: ")     #not sure if how will it work with multiple grades
+    next_id = len(student_data) + 1
     student = {
         "name": name,
         "age": age,
         "grade": grade    }
-    students.append(student)
+    student_data.append(student)
     next_id += 1
-    save_students("grading_py.json", students)
+    save_students("grading_py.json", student_data) #why create save_students?
     print("Student added successfully!")
     
 
@@ -106,4 +86,34 @@ def add_student():
 # task 5 (Harshpreet) -> Checking grades. Outputting grades of a student from DB
     
 
+username_input = input("Enter username:")
+password_input = input("Enter password:")
+
+
+
+
+
+while user_role(username_input, password_input, login_data) == "admin":
+    print("admin login successful")
+
+    action = input("choose action(add_students, add_grades, check_grades): ")
+    if action == "add_students":
+        add_student()
+    elif action == "add_grades":
+        add_grade()
+    elif action == "check_grades":
+#        check_grades()   still to do
+        print("in progress...")
+
+
+
+while user_role(username_input, password_input, login_data) == "user":
+    print("user login successful")
+
+    action = input("choose action(check_grades): ")
+
+
+
+
     
+print("Access denied")
